e631daf9f343e790c444fc097b5e3085b4dd0813
Dan <dan@aredherring.tech>
2021-09-26 23:16:42 +0100

    add detailed failure messages

--- router.test.ts
+++ router.test.ts
@@ -58,18 +58,18 @@ Deno.test({
     let req = new Request(uri.href, {
       method: "get",
     });
-    assertEquals(route.matches(req), true);
+    assertEquals(route.matches(req), true, "failed pattern test");
 
     req = new Request(uri.href, {
       method: "post",
     });
-    assertEquals(route.matches(req), false);
+    assertEquals(route.matches(req), false, "failed method tes");
 
     uri = new URL("/1234/5567", TEST_BASE_URL);
     req = new Request(uri.href, {
       method: "get",
     });
-    assertEquals(route.matches(req), false);
+    assertEquals(route.matches(req), false, "failed nested pattern test");
   },
 });
 
